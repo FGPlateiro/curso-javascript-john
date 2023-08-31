cce72e6041e934ae214a8d5a53792e9360a5073a
Felipe Gomes Plateiro <felipe.plateiro@gmail.com>
2023-08-31 19:34:30 -0300

    adicionado exercicio 8 completo e a base do exercicio 9

--- aula2/exercicios/exercicio8.js
+++ aula2/exercicios/exercicio8.js
@@ -2,3 +2,18 @@
  * Conversão de Notas: Transforme suas notas em símbolos legais! 
  * Crie um programa que atribua letras (como A, B, C) para as notas que você obteve em um teste.
  */
+
+const nota = 3;
+
+if (nota < 0){
+    console.log (" nota invalido");
+} 
+ if (nota <= 3){
+    console.log ("Sua nota é C");
+}
+ if (nota > 3 && nota <= 7){
+    console.log ("sua nota é B");
+}
+ if (nota > 7 ){
+    console.log (" sua nota é A")
+}
--- aula2/exercicios/exercicio9.js
+++ aula2/exercicios/exercicio9.js
@@ -1,5 +1,113 @@
 /**
- * Validação de Senha: Construa um guarda de segurança digital! 
- * Crie um programa que verifica se a senha que você escolheu é forte o suficiente para proteger suas coisas.
- * uma senha forte o suficiente deve ter ao menos 8 digitos, conter números e letras maíusculas e minúsculas
+ * Validação de password: Construa um guarda de segurança digital! 
+ * Crie um programa que verifica se a password que você escolheu é forte o suficiente para proteger suas coisas.
+ * uma password forte o suficiente deve ter ao menos 8 digitos, conter números e letras maíusculas e minúsculas
  */
+
+const letter = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "x",
+    "z",
+    "y",
+]
+
+const letterUP = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "X",
+    "Z",
+    "Y",
+]
+
+const number = [
+    0,
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+]
+
+const especialChar = [
+    "@",
+    "#",
+    "$",
+    "%",
+    "_",
+    "&",
+    "*",
+    "ç",
+    "/",
+    "<",
+    ">",
+    "¨",
+    "^",
+    "~",
+    "-",
+    "+",
+    ".",
+]
+
+const password = "Plateiro@1234";
+
+let letterQuantity = 0;
+
+let numberQuantity = 0;
+
+let letterUp = 0;
+
+let especialCharQuantity = 0;
+
+const passwordSize = password.length;
+
+     
+    if (password.lenght < 8){
+
+        
+    }
+    
